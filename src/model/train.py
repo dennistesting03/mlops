2a7427c1c8370dc87f58d7e3c1086945cf5f6194
dennistesting03 <dennistesting03@gmail.com>
2025-10-30 20:43:57 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -7,6 +7,7 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
@@ -34,6 +35,23 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(data, target_column, test_size=0.2, random_state=42):
+    """
+    Splits the dataset into training and test sets.
+
+    Parameters:
+    - data (pd.DataFrame): The input dataset.
+    - target_column (str): The name of the target column.
+    - test_size (float): Proportion of the dataset to include in the test split.
+    - random_state (int): Random seed for reproducibility.
+
+    Returns:
+    - X_train, X_test, y_train, y_test: Split datasets.
+    """
+    X = data.drop(columns=[target_column])
+    y = data[target_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
